2f150bc5cbdfea68b94dd52a4f91a808047fef6d
Ryan Keith <ryankeithster@gmail.com>
2022-03-20 16:28:22 -0700

    Initial commit

---
+++ VideoFrameCaptureLibrary/FrameReaderBuilder.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.MediaProperties;
+
+namespace VideoFrameCaptureLibrary
+{
+    /// <summary>
+    /// Builds a MediaFrameReader object that can receive video frames from the specified system camera device.
+    /// </summary>
+    public class FrameReaderBuilder
+    {
+        private MediaCapture captureDevice;
+
+        private uint width;
+        private uint height;
+        private string? mediaEncoding;
+        private string? cameraName;
+        private string? deviceId;
+
+        public static string VideoEncodingNV12
+        {
+            get => MediaEncodingSubtypes.Nv12;
+        }
+
+        public uint PreferredWidth
+        {
+            get => width;
+            set => width = value;
+        }
+        public uint PreferredHeight
+        {
+            get => height;
+            set => height = value;
+        }
+        public string? MediaEncoding
+        {
+            get => mediaEncoding;
+            set => mediaEncoding = value;
+        }
+
+        public string? CameraName
+        {
+            get => cameraName;
+            set => cameraName = value;
+        }
+
+        public FrameReaderBuilder(string cameraName, uint preferredWidth, uint preferredHeight, string encoding)
+        {
+            captureDevice = new MediaCapture();
+            CameraName = cameraName;
+            PreferredHeight = preferredHeight;
+            PreferredWidth = preferredWidth;
+            MediaEncoding = (!string.IsNullOrEmpty(encoding) ? encoding : string.Empty);
+        }
+
+        public async Task<MediaFrameReader?> Build()
+        {
+            if (CameraName == null)
+            {
+                throw new ArgumentNullException(nameof(CameraName));
+            }
+
+            // Find the system device id for the specified camera
+            deviceId = await GetCameraDeviceId(CameraName);
+
+            if (string.IsNullOrEmpty(deviceId))
+            {
+                Console.WriteLine("Could not find system camera device with the specified name");
+                return null;
+            }
+
+            MediaFrameSourceGroup sourceGroup = await MediaFrameSourceGroup.FromIdAsync(deviceId);
+
+            var settings = new MediaCaptureInitializationSettings()
+            {
+                SourceGroup = sourceGroup,
+                SharingMode = MediaCaptureSharingMode.ExclusiveControl,
+                MemoryPreference = MediaCaptureMemoryPreference.Cpu,
+                StreamingCaptureMode = StreamingCaptureMode.Video
+            };
+
+            // Create and initialize the media capture device instance representing
+            // the camera
+            var mediaCaptureDevice = new MediaCapture();
+            try
+            {
+                await mediaCaptureDevice.InitializeAsync(settings);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("MediaCapture initialization failed: " + ex.Message);
+                return null;
+            }
+
+            // Get reference that will allow us to manipulate settings related to color video
+            MediaFrameSourceInfo colorSourceInfo = sourceGroup.SourceInfos[0];
+
+            // If supported, this is how you would get references that let you manipulate settings related
+            // to depth and IR
+            //MediaFrameSourceInfo infraredSourceInfo = sourceGroup.SourceInfos[1];
+            //MediaFrameSourceInfo depthSourceInfo = sourceGroup.SourceInfos[2];
+
+            // Set captured video format
+            bool videoFormatSetSuccess = await SetPreferredVideoFormat(mediaCaptureDevice, colorSourceInfo);
+
+            if (!videoFormatSetSuccess)
+            {
+                Console.WriteLine("Unable to set the captured video format to the specified format");
+                return null;
+            }
+
+            // Create a reader to receive video frames
+            MediaFrameSource colorFrameSource = mediaCaptureDevice.FrameSources[colorSourceInfo.Id];
+            MediaFrameReader frameReader = await mediaCaptureDevice.CreateFrameReaderAsync(colorFrameSource);
+
+            return frameReader;
+        }
+
+        private async Task<string> GetCameraDeviceId(string CameraName)
+        {
+            string deviceId = string.Empty;
+
+            // Finds all video capture devices
+            DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+            foreach (DeviceInformation device in devices)
+            {
+                if (device.Name.Contains(CameraName))
+                {
+                    deviceId = device.Id;
+                    break;
+                }
+            }
+
+            return deviceId;
+        }
+
+        private async Task<bool> SetPreferredVideoFormat(MediaCapture captureDevice, MediaFrameSourceInfo colorSourceInfo)
+        {
+            MediaFrameSource colorFrameSource = captureDevice.FrameSources[colorSourceInfo.Id];
+
+            MediaFrameFormat? preferredFormat = colorFrameSource.SupportedFormats.Where(format =>
+            {
+                return format.VideoFormat.Width >= PreferredWidth
+                && format.Subtype.Equals(MediaEncoding, StringComparison.CurrentCultureIgnoreCase);
+
+            }).FirstOrDefault();
+
+            if (preferredFormat == null)
+            {
+                // Our desired format is not supported
+                return false;
+            }
+
+            await colorFrameSource.SetFormatAsync(preferredFormat);
+            return true;
+        }
+
+    }
+}
---
+++ VideoFrameCaptureLibrary/VideoFrameReader.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture.Frames;
+using Windows.Storage.Streams;
+
+namespace VideoFrameCaptureLibrary
+{
+    public class VideoFrameReader
+    {
+        private FrameReaderBuilder _frameReaderBuilder;
+        private MediaFrameReader? _frameReader;
+        public VideoFrameReader()
+        {
+            //string cameraName = "C615";
+            string cameraName = "Surface";
+
+            // Build a video frame reader to capture 720p video.
+            // NV12 seems to be the preferred format. Attempting to use MJPG instead for instance still results in NV12 imagery.
+            // NV12 is similar to YUV420: YUV color space with 420 chroma subsampling
+            _frameReaderBuilder = new FrameReaderBuilder(cameraName, 1280, 720, FrameReaderBuilder.VideoEncodingNV12);
+        }
+
+        public async Task InitializeAsync()
+        {
+            _frameReader = await _frameReaderBuilder.Build();
+        }
+
+        public async void StartCapture()
+        {
+            if (_frameReader != null)
+            {
+                _frameReader.FrameArrived += ColorFrameReader_FrameArrivedAsync;
+                await _frameReader.StartAsync();
+            }
+        }
+
+        /// <summary>
+        /// Grab the latest video frame, convert it and write it to disk as a JPEG image.
+        /// </summary>
+        /// <seealso cref="https://stackoverflow.com/questions/34291291/how-to-get-byte-array-from-softwarebitmap"/>
+        /// <seealso cref="https://docs.microsoft.com/en-us/windows/uwp/audio-video-camera/process-media-frames-with-mediaframereader"/>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private static async void ColorFrameReader_FrameArrivedAsync(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+        {
+            // Grab the current timestamp immediately. This will be used to generate a unique
+            // output file name, and waiting until later in the method to grab the timestamp
+            // can result in resource contention as other threads grab the same timestamp and
+            // try and create a file that has already been created by previous threads
+            DateTime dtNow = DateTime.UtcNow;
+
+            Console.WriteLine(DateTime.Now.Millisecond.ToString() + " " + Thread.CurrentThread.ManagedThreadId + " ==> Frame received");
+
+            StringBuilder filePath = new StringBuilder(@"C:\temp\video_frames\");
+            MediaFrameReference mediaFrameReference;
+            if ((mediaFrameReference = sender.TryAcquireLatestFrame()) != null)
+            {
+                Console.WriteLine(DateTime.Now.Millisecond.ToString() + " " + Thread.CurrentThread.ManagedThreadId + " ==> Acquired latest frame");
+                VideoMediaFrame videoFrame = mediaFrameReference.VideoMediaFrame;
+
+                if (videoFrame != null)
+                {
+                    byte[] frameBytes;
+                    using (var ms = new InMemoryRandomAccessStream())
+                    {
+                        // Get encoder to convert the frame to JPEG
+                        BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, ms);
+
+                        // The video frame will likely be in NV12 format. NV12 uses a YUV color space instead of an RGB color space.
+                        // BitmapEncoder will only work with imagery in RGB color space, so convert to RGB first
+                        SoftwareBitmap interimRgbSwBmp = SoftwareBitmap.Convert(videoFrame.SoftwareBitmap, BitmapPixelFormat.Rgba8);
+
+                        encoder.SetSoftwareBitmap(interimRgbSwBmp);
+
+                        try
+                        {
+                            // We don't want to encode any more frames so commit it
+                            await encoder.FlushAsync();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(DateTime.Now.Millisecond.ToString() + " " + Thread.CurrentThread.ManagedThreadId + " ==> Caught exception");
+                            return;
+                        }
+
+                        frameBytes = new byte[ms.Size];
+                        await ms.ReadAsync(frameBytes.AsBuffer(), (uint)ms.Size, InputStreamOptions.None);
+
+                        // frameBytes now contains the JPEG data; write it out!
+                        string fileName = @"C:\temp\video_frames\" + dtNow.Hour.ToString() + "h_" + dtNow.Minute.ToString() + "m_" + dtNow.Second.ToString() + "s_" + dtNow.Millisecond.ToString() + "ms.jpg";
+                        File.WriteAllBytes(fileName, frameBytes);
+                    }
+
+                }
+            }
+        }
+    }
+}
